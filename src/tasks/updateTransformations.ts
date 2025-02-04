74c344341668f9d2ecabc2cc88f45bd259d7fedd
Elmo Egers <uplaymedia@gmail.com>
2025-02-04 22:51:31 +0200

    fix: handle edge cases that may occur when the file is replaced by the user during eager transformation generation

--- src/tasks/updateTransformations.ts
+++ src/tasks/updateTransformations.ts
@@ -1,5 +1,5 @@
 import type { ConfigOptions, UploadApiOptions } from 'cloudinary'
-import type { CollectionSlug, TaskConfig } from 'payload'
+import type { CollectionSlug, PayloadRequest, TaskConfig } from 'payload'
 
 import type { CloudinaryFile } from '../types.js'
 
@@ -7,13 +7,18 @@ import { CloudinaryService } from '../services/CloudinaryService.js'
 
 /**
  * Task configuration for updating transformations of a Cloudinary file.
- * This task handles the process of updating transformations for a given image or video file in the Cloudinary
+ * This task handles the process of updating transformations for a given image or video in the Cloudinary
  * product environment and ensures that the document's state is updated in the database accordingly.
+ *
+ * @type {TaskConfig<'updateTransformations'>}
+ * @property {string} slug - The unique identifier for the task.
+ * @property {Function} handler - The function that handles the task execution by calling the updateTransformations method.
+ * @property {Array} inputSchema - The schema defining the input parameters for the task.
+ * @property {number} retries - The number of retries allowed for the task.
  */
 export const updateTransformationsTask: TaskConfig<'updateTransformations'> = {
   slug: 'updateTransformations',
   handler: async ({ input, req }) => {
-    let hasErrors = false
     const { collection, configOptions, doc, previousDoc, uploadApiOptions } = input as unknown as {
       collection: CollectionSlug
       configOptions?: ConfigOptions
@@ -21,47 +26,23 @@ export const updateTransformationsTask: TaskConfig<'updateTransformations'> = {
       previousDoc: CloudinaryFile | null
       uploadApiOptions?: UploadApiOptions
     }
+    const cldService = new CloudinaryService(configOptions, uploadApiOptions)
 
     // Update transformations
     try {
-      const cldService = new CloudinaryService(configOptions, uploadApiOptions)
+      const docUpdated = await updateDoc({ collection, doc, hasErrors: false, isProcessing: true, req })
 
-      await req.payload.db.updateOne({
-        collection,
-        data: {
-          isProcessing: true
-        },
-        where: {
-          publicId: {
-            equals: doc.publicId ?? -1
-          }
-        }
-      })
-
-      await cldService.updateTransformations({ doc, previousDoc, req })
+      if (docUpdated) {
+        await cldService.updateTransformations({ doc, previousDoc, req })
+        await updateDoc({ collection, doc, hasErrors: false, isProcessing: false, req })
+      }
     } catch (err) {
-      req.payload.logger.error({ err, msg: 'Unexpected error in updateTransformations' })
-      hasErrors = true
-    }
+      const docUpdated = await updateDoc({ collection, doc, hasErrors: true, isProcessing: false, req })
 
-    const isProcessing = false
-
-    // Update file state
-    try {
-      await req.payload.db.updateOne({
-        collection,
-        data: {
-          hasErrors,
-          isProcessing
-        },
-        where: {
-          publicId: {
-            equals: doc.publicId ?? -1
-          }
-        }
-      })
-    } catch (err) {
-      req.payload.logger.error({ err, msg: 'Failed to update file in updateTransformations' })
+      // If the underlying file did not change, log the error
+      if (docUpdated) {
+        req.payload.logger.error({ err, msg: 'Unexpected error in updateTransformations task.' })
+      }
     }
 
     return {
@@ -94,4 +75,85 @@ export const updateTransformationsTask: TaskConfig<'updateTransformations'> = {
     }
   ],
   retries: 0
+}
+
+/**
+ * Retrieves a document from the specified collection based on the provided publicId.
+ * This function queries the database to find a document in the given collection where the publicId matches the provided value.
+ *
+ * @param {Object} args - Method arguments.
+ * @param {string} args.collection - The name of the collection to query.
+ * @param {null | string | undefined} args.publicId - The publicId of the document to retrieve.
+ * @param {PayloadRequest} args.req - The request object.
+ * @returns {Promise<CloudinaryFile | null>} - A promise that resolves with the retrieved CloudinaryFile document or null if it does not exist.
+ */
+function getDoc({
+  collection,
+  publicId,
+  req
+}: {
+  collection: string
+  publicId: null | string | undefined
+  req: PayloadRequest
+}): Promise<CloudinaryFile | null> {
+  return req.payload.db.findOne<CloudinaryFile>({
+    collection,
+    where: {
+      publicId: {
+        equals: publicId ?? '-1'
+      }
+    }
+  })
+}
+
+/**
+ * Updates a document in the specified collection based on the provided publicId.
+ * This function checks if the document exists in the collection and updates its attributes if found.
+ * If the document does not exist, it logs a warning message.
+ *
+ * @param {Object} args - Method arguments.
+ * @param {string} args.collection - The name of the collection to update.
+ * @param {CloudinaryFile} args.doc - The document to be updated.
+ * @param {boolean} args.hasErrors - Indicates if there are errors in the document.
+ * @param {boolean} args.isProcessing - Indicates if the document is being processed.
+ * @param {PayloadRequest} args.req - The request object.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the document was updated, false otherwise.
+ */
+async function updateDoc({
+  collection,
+  doc,
+  hasErrors,
+  isProcessing,
+  req
+}: {
+  collection: string
+  doc: CloudinaryFile
+  hasErrors: boolean
+  isProcessing: boolean
+  req: PayloadRequest
+}): Promise<boolean> {
+  const existingDoc = await getDoc({ collection, publicId: doc.publicId, req })
+
+  if (existingDoc) {
+    await req.payload.db.updateOne({
+      collection,
+      data: {
+        hasErrors,
+        isProcessing
+      },
+      where: {
+        publicId: {
+          equals: doc.publicId
+        }
+      }
+    })
+
+    return true
+  } else {
+    req.payload.logger.warn({
+      msg: `Unable to update document with publicId ${doc.publicId} as it has been deleted from the database.`
+    })
+  }
+
+  return false
 }
